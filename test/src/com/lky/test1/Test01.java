340d689e0a463d4bbc53eace13f6ce0eb65d18f6
LKY <liukangye2020@outlook.com>
2022-12-31 23:56:42 +0800

    Initial commit

---
+++ test/src/com/lky/test1/Test01.java
@@ -0,0 +1,26 @@
+package com.lky.test1;
+
+/**
+ * @author shkstart
+ * @Description
+ * @date 2022-11-06-22:10
+ */
+public class Test01 {
+
+        public int computing(int x, int y)
+        {
+            int result;
+            if (x < 5 && y == 5) {
+                result = x + y;
+            } else {
+                result= x / y;
+            }
+            return result;
+        }
+
+    public static void main(String[] args) {
+        Test01 test01 = new Test01();
+        int computing = test01.computing(6, 6);
+        System.out.println(computing);
+    }
+}
